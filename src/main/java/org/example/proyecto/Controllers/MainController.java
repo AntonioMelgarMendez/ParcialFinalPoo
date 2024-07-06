1e592ff1b2ae645dcf79873cea863aeae4d654b7
Yael2222 <00038623@uca.edu.sv>
2024-07-05 21:28:23 -0600

    completando tareas de yael sin comentarios

--- src/main/java/org/example/proyecto/Controllers/MainController.java
+++ src/main/java/org/example/proyecto/Controllers/MainController.java
@@ -1,14 +1,36 @@
 package org.example.proyecto.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void reportea(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        SceneChanger.changeScene(stage,"/org/example/proyecto/ViewsFXML/ReportA.fxml");
+    }
+    @FXML
+    private void reporteb(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        SceneChanger.changeScene(stage,"/org/example/proyecto/ViewsFXML/ReportB.fxml");
+    }
+    @FXML
+    private void reportec(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        SceneChanger.changeScene(stage,"/org/example/proyecto/ViewsFXML/ReportC.fxml");
+    }
+    @FXML
+    private void reported(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        SceneChanger.changeScene(stage,"/org/example/proyecto/ViewsFXML/ReportD.fxml");
+    }
+
+    @FXML
+    private void salir(ActionEvent event){
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
---
+++ src/main/java/org/example/proyecto/Transaccion.java
@@ -0,0 +1,61 @@
+package org.example.proyecto;
+
+import java.sql.Date;
+
+public class Transaccion {
+
+    private int idTransaccion;
+    private Date fechaCompra;
+    private double totalMonto;
+    private String descripcion;
+    private int idCliente;
+
+    public Transaccion(int idTransaccion, Date fechaCompra, double totalMonto, String descripcion, int idCliente) {
+        this.idTransaccion = idTransaccion;
+        this.fechaCompra = fechaCompra;
+        this.totalMonto = totalMonto;
+        this.descripcion = descripcion;
+        this.idCliente = idCliente;
+    }
+
+    // Getters and Setters
+    public int getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public void setIdTransaccion(int idTransaccion) {
+        this.idTransaccion = idTransaccion;
+    }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public double getTotalMonto() {
+        return totalMonto;
+    }
+
+    public void setTotalMonto(double totalMonto) {
+        this.totalMonto = totalMonto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+}
