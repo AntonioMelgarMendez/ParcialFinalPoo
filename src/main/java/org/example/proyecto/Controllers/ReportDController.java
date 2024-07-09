a55976291d495c8e0b6e236c14b890d7d9d560f4
darlyndonis <00083823@uca.edu.sv>
2024-07-08 18:12:02 -0600

    Adding Controller D y fxml

--- src/main/java/org/example/proyecto/Controllers/ReportDController.java
+++ src/main/java/org/example/proyecto/Controllers/ReportDController.java
@@ -1,4 +1,37 @@
 package org.example.proyecto.Controllers;
 
-public class ReportDController { //
-}
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import org.example.proyecto.Tables.Tarjeta;
+import org.example.proyecto.Utilities.AlertsManager;
+import org.example.proyecto.Utilities.DataBaseCredentials;
+import org.example.proyecto.Utilities.SceneChanger;
+
+import java.sql.*;
+
+import static org.example.proyecto.Utilities.CleanData.limpiarDatos;
+import static org.example.proyecto.Utilities.SaveTXT.SaveAReport;
+import static org.example.proyecto.Utilities.SaveTXT.SaveCReport;
+
+public class ReportDController { // 00083823 Controller para
+    @FXML
+    private ComboBox<String> comboBoxFacilitador; // 00083823 ComboBox para seleccionar el facilitador de tarjeta
+
+    @FXML
+    private Button btnBuscar; // 00083823 Botón para buscar los datos de las transacciones
+
+    @FXML
+    private Button btnLimpiar; // 00083823 Botón para limpiar los datos mostrados
+
+    @FXML
+    private Button btnSalir; // 00083823 Botón para Salir de la vista del reporte D
+
+
+    }
