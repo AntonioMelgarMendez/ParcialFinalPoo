c79bbeb2b2b803cc082909fb821552bed5afd246
Daniel Mendez <AntonioMelgarMendez>
2024-07-08 19:10:44 -0600

    Fixing comments

--- src/main/java/org/example/proyecto/Utilities/SceneChanger.java
+++ src/main/java/org/example/proyecto/Utilities/SceneChanger.java
@@ -6,23 +6,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import static org.example.proyecto.Utilities.AlertsManager.showAlert;
-
-//00009123 Clase para manejar el cambio de escena
-public class SceneChanger {
-    //00009123 Metodo para cambiar de escena con el stage y el fxml al que se desea cambiar
-    public static void changeScene(Stage stage, String fxmlPath) {
+public class SceneChanger {//00009123 Clase para manejar el cambio de escena
+    public static void changeScene(Stage stage, String fxmlPath) {//00009123 Metodo para cambiar de escena con el stage y el fxml al que se desea cambiar
         try {
-            //00009123 Creamos el FXMLLoader con la dirrecion proporcionada
-            FXMLLoader fxmlLoader = new FXMLLoader(SceneChanger.class.getResource(fxmlPath));
-            //00009123 Creamos la nueva escena
-            Scene scene = new Scene(fxmlLoader.load());
-            //00009123 Seteamos la nueva escena
-            stage.setScene(scene);
-            //00009123 Mostramos la nueva escena
-            stage.show();
-        } catch (IOException e) {
-            //00009123 Capturamos la excepcion para mostrar la alerta pasandole la causa
-            showAlert("Error cambiar de escena", "Cambio fallido" + e.getCause(), e.getMessage());
+            FXMLLoader fxmlLoader = new FXMLLoader(SceneChanger.class.getResource(fxmlPath));//00009123 Creamos el FXMLLoader con la dirrecion proporcionada
+            Scene scene = new Scene(fxmlLoader.load()); //00009123 Creamos la nueva escena
+            stage.setScene(scene); //00009123 Seteamos la nueva escena
+            stage.show();//00009123 Mostramos la nueva escena
+        } catch (IOException e) {//00009123 Capturamos la excepcion para mostrar la alerta pasandole la causa
+            showAlert("Error cambiar de escena", "Cambio fallido" + e.getCause(), e.getMessage());//00009123 Mostramos un mensaje de error
         }
     }
 }
